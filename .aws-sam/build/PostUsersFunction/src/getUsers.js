36e263a1128e1dc3eef94aaae7598640604ba402
quanghuy1 <huy.tranquang1@vti.com.vn>
2025-08-09 21:48:25 +0900

    update template fix cors post

--- .aws-sam/build/PostUsersFunction/src/getUsers.js
+++ .aws-sam/build/PostUsersFunction/src/getUsers.js
@@ -2,10 +2,17 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME;
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Requested-With',
+  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+};
+
 exports.handler = async () => {
   const data = await docClient.scan({ TableName: TABLE_NAME }).promise();
   return {
     statusCode: 200,
+    headers: headers,
     body: JSON.stringify(data.Items),
   };
 };
