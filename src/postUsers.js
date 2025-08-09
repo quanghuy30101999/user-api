dc097f0cf0401b294e78333e4ccf1e3e12e00145
quanghuy1 <huy.tranquang1@vti.com.vn>
2025-08-09 10:42:29 +0900

    config cors

--- src/postUsers.js
+++ src/postUsers.js
@@ -3,6 +3,12 @@ const { v4: uuidv4 } = require('uuid');
 const docClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME;
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Requested-With',
+  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+};
+
 exports.handler = async (event) => {
   const { name } = JSON.parse(event.body);
   const id = uuidv4();
@@ -12,6 +18,7 @@ exports.handler = async (event) => {
 
   return {
     statusCode: 201,
+    headers: headers,
     body: JSON.stringify(newUser),
   };
 };
